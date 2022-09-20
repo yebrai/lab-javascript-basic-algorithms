1701af78f5057749ba9820ccb57c5356ca794206
Yebrai <ivansevv@gmail.com>
2022-09-20 20:08:28 +0200

    solved itarion 1 and 2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,52 @@
+console.log("i'm ready!")
+
 // Iteration 1: Names and Input
+
+let hacker1 = "Yebrai"
+
+console.log(`The driver's name is ${hacker1}`)
+
+let hacker2 = "Ironhack"
+
+console.log("The navigator's name is " + hacker2)
 
 
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
+} else if (hacker1.length < hacker2.length) {
+    console.log(`t seems that the navigator has the longest name, it has ${hacker2.length} characters`)
+} else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+}
+
 
 // Iteration 3: Loops
+let nameUpper = ""
+for (let i = 0; i < hacker1.length; i++) {
+    nameUpper += hacker1[i].toUpperCase()
+  if (nameUpper[i] = String ) {
+    nameUpper += " "
+  }
+}
+console.log(nameUpper)
+
+//3.2
+let nameReverse = ""
+for (let i = hacker1.length; 0 < hacker1.length; i--) {
+    nameReverse += hacker1[i]
+}
+
+//3.3
+let hacker1 = "Yebrai"
+let hacker2 = "Ironhack"
+
+let driverLexi = ""
+let naviLexi = ""
+
+for (let i = 0; i < hacker1.length; i++) {
+  driverLexi = driverLexi + driverLexi[i]
+
+  }
+console.log(driverLexi)
